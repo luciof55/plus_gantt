688199861940ba32505a6000f56ca0e30d702ce3
Top Group <gitlab@topgroup.com.ar>
2020-01-22 13:03:27 -0300

    Specify version of Active Record / Rails

--- db/migrate/002_create_pg_tracker_config.rb
+++ db/migrate/002_create_pg_tracker_config.rb
@@ -1,4 +1,4 @@
-class CreatePgTrackerConfig < ActiveRecord::Migration
+class CreatePgTrackerConfig < Rails.version < '5.1' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
     create_table :pg_tracker_configs do |t|
       t.belongs_to :tracker, :index => true, :null => false
